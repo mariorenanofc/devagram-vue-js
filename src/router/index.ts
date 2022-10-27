a49630f6d84247f650c1b878756a900a53f735b2
mariorenanofc <mariorenan25@gmail.com>
2022-10-26 22:51:51 -0300

    Costomização do perfil

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,6 +5,7 @@ import CadastroVue from '@/views/Cadastro.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import UsuarioVue from '@/views/Usuario.vue';
 import PerfilVue from '@/views/Perfil.vue';
+import EditarVue from '@/views/Editar.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -36,6 +37,12 @@ const router = createRouter({
       path: '/perfil/:id',
       name: 'perfil',
       component: PerfilVue,
+      meta: {rotaPrivada: true}
+    },
+    {
+      path: '/editar',
+      name: 'editar',
+      component: EditarVue,
       meta: {rotaPrivada: true}
     }
     
---
+++ src/views/Editar.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { defineComponent } from 'vue';
+    import Header from '../components/Header.vue';
+    import Footer from '../components/Footer.vue';
+    import { UsuarioServices } from '@/services/UsuarioServices';
+    import HeaderAcoes from '@/components/HeaderAcoes.vue';
+    
+    const usuarioServices = new UsuarioServices();
+    
+    export default defineComponent({
+        components: { Header, Footer, HeaderAcoes },
+        data(){
+            return {
+            }
+        }
+    });
+</script>
+
+
+<template>
+    <Header :hidden="true" />
+    <HeaderAcoes :showLeft="false"
+        :showRight="true" :isRightIcon="true" title="Editar Perfil" />
+    <Footer />
+</template>
