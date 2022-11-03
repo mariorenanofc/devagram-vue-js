b39c2b20476f907926165b9c837cacc0e2d0b047
mariorenanofc <mariorenan25@gmail.com>
2022-11-03 17:23:02 -0300

    editar dados e estilos do usuario

--- src/views/Editar.vue
+++ src/views/Editar.vue
@@ -33,13 +33,15 @@ export default defineComponent({
         <HeaderAcoes :showLeft="true" :showRight="true" rightLabel=" Concluir" title="Editar Perfil" />
         <Avatar :imagem="getImagem" />
         <button>Alterar Foto de Perfil</button>
-        <input type="file" class="oculto" accept="image/" ref="referenciaInput" />
+        <input type="file" class="oculto" accept="image/*" ref="referenciaInput" />
         <div class="input">
             <span>Nome</span>
-            <input type="text" v-model="nome" placeholder="Informe seu noma">
+            <input type="text" v-model="nome" placeholder="Informe seu nome">
             <img src="../assets/imagens/limpar.svg" alt="limpar" />
         </div>
         <Footer />
     </div>
 
 </template>
+
+<style lang="scss" src="@/assets/styles/editar.scss" />
